41d89c1aa04e3ede3b40ded5fa773c07e09b0c94
skhandelwal758 <65276353+skhandelwal758@users.noreply.github.com>
2023-03-01 21:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-//your code here
+var btn = document.getElementById('addTodoBtn');
+btn.addEventListener('click', ()=>{
+	var text = document.getElementById('newTodoInput');
+	var value = text.value;
+    // console.log(text);
+	let ui = `<li>${value}</li>`;
+	let list = document.getElementById('todoList');
+	list.innerHTML+=ui;
+	
+	document.getElementById('newTodoInput').value = "";
+	
+	
+});
+
+	
 
