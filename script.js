d91095fba7e8c2ee963fce3ebb04b45137cd9f1a
skhandelwal758 <65276353+skhandelwal758@users.noreply.github.com>
2023-03-01 21:57:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ var btn = document.getElementById('addTodoBtn');
 btn.addEventListener('click', ()=>{
 	var text = document.getElementById('newTodoInput');
 	var value = text.value;
+    if(value!=""){
     // console.log(text);
 	let ui = `<li>${value}</li>`;
 	let list = document.getElementById('todoList');
 	list.innerHTML+=ui;
 	
 	document.getElementById('newTodoInput').value = "";
+    } else{
+        console.log("invalid!!");
+    }
 	
 	
 });
 
-	
 
